65ced226ba32c7da51609bc502d4e57d345b6413
William Stull II <williamstull@gmail.com>
2021-11-16 15:46:14 -0800

    Homepage/login page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,11 +7,14 @@ export default function HomePage() {
     return (
        <div>
             <div class="container" style={{ backgroundImage: "url(/fireside.png)" } }>
-                <h1 class="title">FIRESIDE</h1>
-                <h3 class="stupid-inspirational-quote">Live, Laugh, Love</h3>
-                <Link to='/Login'><button class="btn" >Login</button></Link>
-                <Link to='/SignUp'><button class="btn" >Sign-Up</button></Link>
-                
+                <div class="fuzz">
+                    <h1 class="title">FIRESIDE</h1>
+                    <h3 class="stupid-inspirational-quote">Live, Laugh, Love</h3>
+                </div>
+                <div class="btn-container">
+                    <Link to='/Login'><button class="btn" >Login</button></Link>
+                    <Link to='/SignUp'><button class="btn" >Sign-Up</button></Link>
+                </div>
             </div>
         </div>
     )
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,12 +2,53 @@ import React, { useState } from 'react';
 import './LoginPage.scss';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage'
 import userService from '../../utils/userService';
+import { useHistory } from "react-router-dom";
 
 
 export default function LoginPage(props){
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    });
+    const [error, setError] = useState('');
+    const history = useHistory();
 
+    function handleChange(evt) {
+        setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
+        setError('');
+    }
+
+    async function handleSubmit(evt) {
+        // Prevent form from being submitted to the server
+        evt.preventDefault();
+        try {
+          // The promise returned by the signUp service method 
+          // will resolve to the user object included in the
+          // payload of the JSON Web Token (JWT)
+          const user = await userService.login(credentials);
+          //setUser(user);
+          history.push("/home");
+        } catch {
+          setError('Log In Failed - Try Again');
+        }
+    }
 
     return (
-             <h1>Login PAGE</h1>
+        <div>
+            <h1>Login PAGE</h1>
+            <div className="auth-body">
+                <div className="form-container" onSubmit={handleSubmit}>
+                    <form autoComplete="off" >
+                    <label>Email</label>
+                    <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
+                    <label>Password</label>
+                    <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+                    <button type="submit">LOG IN</button>
+                    </form>
+                </div>
+                <p className="error-message"><br/>{error}</p>
+            </div>
+        </div>
+             
       );
 }
