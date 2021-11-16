6b191375b6f6526f7422cc37850d33286ad1ee20
Cat <morancathy@gmail.com>
2021-11-16 15:20:43 -0600

    signup routes and functionality

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGODB_URI
+const db = mongoose.connection;
 
-mongoose.connect(
-  process.env.DATABASE_URL,
+mongoose.connect(MONGODB_URI, 
   { useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-  }
-);
-
-const db = mongoose.connection;
+  });
 
 db.on('connected', function() {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express');
+var cors = require('cors')
 const path = require('path');
 const logger = require('morgan');
 const favicon = require('serve-favicon');
-
+const { signup, login } = require('./controllers/auth');
 require('./config/database');
 
 // Require controllers here
 
 const app = express();
-
+app.use(cors())
 // add in when the app is ready to be deployed
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(logger('dev'));
@@ -20,16 +21,21 @@ app.use(express.static(path.join(__dirname, 'build'))); // this allows express t
 // Configure the auth middleware
 // This decodes the jwt token, and assigns
 // the user information to req.user
-app.use(require('./config/auth')); 
+app.use(require('./config/auth'));
 // api routes must be before the "catch all" route
 //app.use('/api/users', require('./routes/api/users'));
+
+app.post('/api/signup', signup);
+app.post('/api/login', login);
+app.use('/api/signup', require('./controllers/userController'));
+
 
 // "catch all" route
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 2021;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log(`Express app listening on port ${port}`);
