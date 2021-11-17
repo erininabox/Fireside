9085b4d619d76ca6e160a69a2c924c4b3cb00daf
erininabox <erininabox@gmail.com>
2021-11-16 16:36:22 -0800

    some css fixins

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,11 +9,11 @@ export default function HomePage() {
             <div class="container" style={{ backgroundImage: "url(/fireside.png)" } }>
                 <div class="fuzz">
                     <h1 class="title">FIRESIDE</h1>
-                    <h3 class="stupid-inspirational-quote">Live, Laugh, Love</h3>
+                    <h3 class="stupid-inspirational-quote">Making intergenerational connections</h3>
                 </div>
                 <div class="btn-container">
-                    <Link to='/Login'><button class="btn" >Login</button></Link>
-                    <Link to='/SignUp'><button class="btn" >Sign-Up</button></Link>
+                    <Link to='/Login' className="link"><button class="btn" >Login</button></Link>
+                    <Link to='/SignUp' className="link"><button class="btn" >Sign-Up</button></Link>
                 </div>
             </div>
         </div>
